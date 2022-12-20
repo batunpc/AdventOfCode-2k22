272ca6b5e0612cecd1ecd103e92d814e943beed2
batunpc <batuhnpc@gmail.com>
2022-12-20 00:37:31 -0500

    completed Day1

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,34 +1,29 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
-// 1. Sum each number until you see a empty line
-// 2. Store the numbers that are summed into an vector
-// 3. Find the highest number from the vector and print it
-
-std::string caloriePrinter(std::string &lines) { return lines; }
+#include <vector>
 
 int main(int, char **) {
   std::ifstream puzzle_input("input.txt");
-  std::string myline;
-  std::stringstream ss;
-  int value, sum = 0;
+  std::string lines;
+  std::vector<int> sum_of_numbers;
+  int sum_of_cal = 0;
 
   if (puzzle_input.is_open()) {
-    while (puzzle_input) {
-      std::getline(puzzle_input, myline);
-
-      std::stringstream ss;
-      ss.str(myline);
-      ss >> value;
-      std::cout << value << " ";
-      sum += value;
-
-      if (!myline.size())
-        std::cout << "\n";
+    while (std::getline(puzzle_input, lines)) {
+      if (!lines.empty()) {
+        sum_of_cal += std::stoi(lines);
+      } else {
+        sum_of_numbers.push_back(sum_of_cal);
+        sum_of_cal = 0;
+      }
     }
-    std::cout << sum << "\n";
+    sum_of_numbers.push_back(sum_of_cal);
+    puzzle_input.close();
+
+    int max = *std::max_element(sum_of_numbers.begin(), sum_of_numbers.end());
+    std::cout << "Highest Cal: " << max << std::endl;
   } else {
-    std::cout << "Cannot open the puzzle";
+    std::cout << "Unable to open the puzzle";
   }
 }
