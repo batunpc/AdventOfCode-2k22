cc97887b43e37d6bfd21a78e6a82a0fd208caf1c
batunpc <batuhnpc@gmail.com>
2023-01-12 16:47:58 -0500

    day 6 part 2 completed (needs refactoring)

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -3,24 +3,42 @@
 #include <vector>
 
 #include "FileHandler.h"
+#include "Logger.h"
 
-constexpr int packetSize = 4;
+constexpr int packetMarker = 4;
+constexpr int messageMarker = 14;
+
 int main(int argc, char *argv[]) {
   auto lines = sdds::read("input.txt");
   //
 
   for (const auto &line : lines) {
-    for (size_t i = 0; i < line.size() - packetSize; i++) {
-      std::string packet = line.substr(i, packetSize);
+    for (size_t i = 0; i < line.size() - packetMarker; i++) {
+      std::string packet = line.substr(i, packetMarker);
 
       std::set<char> packetSet;
       for (const auto &c : packet) {
         packetSet.insert(c);
       }
 
-      if (packetSet.size() == packetSize) {
-        std::cout << packet << std::endl;
-        std::cout << "i: " << i + packetSize << std::endl;
+      if (packetSet.size() == packetMarker) {
+        challenge::log(i + packetMarker, 1);
+        break;
+      }
+    }
+  }
+
+  for (const auto &line : lines) {
+    for (size_t i = 0; i < line.size() - messageMarker; i++) {
+      std::string packet = line.substr(i, messageMarker);
+
+      std::set<char> packetSet;
+      for (const auto &c : packet) {
+        packetSet.insert(c);
+      }
+
+      if (packetSet.size() == messageMarker) {
+        challenge::log(i + messageMarker, 2);
         break;
       }
     }
